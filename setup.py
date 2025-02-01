deda738bef31b87d6d168d0021076c43aa819564
mistertayodimon <dimondimonych1@outlook.com>
2025-02-01 20:39:37 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='txt_reader',
+    version='0.1.0',
+    description='Библиотека для чтения данных из txt-файлов',
+    author='mistertayodimon',
+    author_email='dimondimonych1@outlook.com',
+    packages=find_packages(),  # Ищет все пакеты (включая txt_reader/)
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
