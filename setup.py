d2982d39b02d69b1494fc1c23dbbbc0ff3ee948b
mistertayodimon <dimondimonych1@outlook.com>
2025-02-02 09:16:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='txt_reader',
-    version='0.1.0',
+    version='0.2.0',
     description='Библиотека для чтения данных из txt-файлов',
     author='mistertayodimon',
     author_email='dimondimonych1@outlook.com',
